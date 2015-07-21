e90cb3f97a1c722e637c8f62b30524b14b129c6d
Stan Janssen <stanjanssen@outlook.com>
2015-07-21 22:36:27 +0200

    different message if no reviews are there

--- views/reviewlist.py
+++ views/reviewlist.py
@@ -1,4 +1,6 @@
 def reviewlist(author, reviews):
+    if not reviews:
+        return 'Sorry buddy, I cannot find any of your reviews in /r/CigarReview\n\n'
     string = "{}'s latest reviews:\n\n".format(author)
     for review in reviews:
         string += '* [{title}]({url})'.format(
