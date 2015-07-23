054bde5d58a9392cc639982b22474b7042796f28
Stan Janssen <stanjanssen@outlook.com>
2015-07-23 22:16:50 +0200

    completed search results view

--- views/reviewlist.py
+++ views/reviewlist.py
@@ -2,14 +2,23 @@ def reviewlist(author, reviews):
     if not reviews:
         return 'Sorry buddy, I cannot find any of your reviews in /r/CigarReview\n\n'
     string = "{}'s latest reviews:\n\n".format(author)
-    for review in reviews:
-        string += '* [{title}]({url})'.format(
-            title = review.title,
-            url   = review.url
-        )
+    string += _create_list(reviews)
     string += '\n\n'
     return string
 
 def searchresults(author, reviews, keyword):
     if not reviews:
         return 'Sorry {}, I cannot find any of your `{}` reviews.\n\n'.format(author, keyword)
+    string = "{}'s latest `{}` reviews:\n\n".format(author, keyword)
+    string += _create_list(reviews)
+    string += '\n\n'
+    return string
+
+def _create_list(review):
+    string = ''
+    for review in reviews:
+        string += '* [{title}]({url})'.format(
+            title = review.title,
+            url   = review.url
+        )
+    return string
